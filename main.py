31c8e0d7e1717945053644e58d9f3c7ff4113a2b
Jessica Zhou <50899049+Haizhuolaojisite@users.noreply.github.com>
2020-05-09 22:56:20 -0600

    Update main.py

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ from models import *
 from utils import progress_bar
 
 
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training') #运行程序时候由于参数不正确，会打印description
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
-args = parser.parse_args()
+args = parser.parse_args()  #Resume the training with `python main.py --resume --lr=0.01`
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
@@ -28,11 +28,11 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop(32, padding=4), #在一个随机的位置进行裁剪,上下左右均填充4个pixel，若为3232，则会变成4040
+    transforms.RandomHorizontalFlip(),  #依据概率p对PIL图片进行水平翻转 参数： p- 概率，默认值为0.5
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+]) #mean and std, 但是为什么是3个值，代表什么呢？
 
 transform_test = transforms.Compose([
     transforms.ToTensor(),
@@ -48,7 +48,9 @@ testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=2)
-
+#num_workers这个参数必须大于等于0，0的话表示数据导入在主进程中进行，其他大于0的数表示通过多个进程来导入数据，可以加快数据导入速度
+#batch_size每次输入数据的行数;PyTorch训练模型时调用数据不是一行一行进行的（这样太没效率），而是一捆一捆来的
+#shuffle(Bool)默认设置是False。将输入数据的顺序打乱，是为了使数据更有独立性，但如果数据是有序列特征的，就不要设置成True了
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
 
@@ -70,15 +72,17 @@ print('==> Building model..')
 net = RegNetX_200MF()
 net = net.to(device)
 if device == 'cuda':
-    net = torch.nn.DataParallel(net)
+    net = torch.nn.DataParallel(net)  #因为pytorch定义的网络模型参数默认放在gpu 0上，所以dataparallel实质是可以看做把训练参数从gpu拷贝到其他的gpu同时训练，此时在dataloader加载数据的时候，batch_size是需要设置成原来大小的n倍，n即gpu的数量。
     cudnn.benchmark = True
+#如果网络的输入数据维度或类型上变化不大，设置  torch.backends.cudnn.benchmark = true  可以增加运行效率；
+#如果网络的输入数据在每次 iteration 都变化的话，会导致 cnDNN 每次都会去寻找一遍最优配置，这样反而会降低运行效率。
 
 if args.resume:
     # Load checkpoint.
     print('==> Resuming from checkpoint..')
-    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    checkpoint = torch.load('./checkpoint/ckpt.pth')
-    net.load_state_dict(checkpoint['net'])
+    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!' #判断某一路径是否为目录
+    checkpoint = torch.load('./checkpoint/ckpt.pth')  # 加载模型文件，pt, pth 文件都可以
+    net.load_state_dict(checkpoint['net']) 
     best_acc = checkpoint['acc']
     start_epoch = checkpoint['epoch']
 
@@ -95,8 +99,8 @@ def train(epoch):
     train_loss = 0
     correct = 0
     total = 0
-    for batch_idx, (inputs, targets) in enumerate(trainloader):
-        inputs, targets = inputs.to(device), targets.to(device)
+    for batch_idx, (inputs, targets) in enumerate(trainloader):  
+        inputs, targets = inputs.to(device), targets.to(device)  #.to(device)是自定义的么？怎么知道是在哪个文件里定义的
         optimizer.zero_grad()
         outputs = net(inputs)
         loss = criterion(outputs, targets)
